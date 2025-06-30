04513df69efb44b24362bd6943d8c3ccd8f96d32
Lýz <147095077+HyperGameDev@users.noreply.github.com>
2025-06-30 16:52:44 -0500

    Joystick visibility types are good

--- mobile_inputs.gd
+++ mobile_inputs.gd
@@ -2,14 +2,24 @@ extends CanvasLayer
 
 @export var joystick_type: joystick_types
 enum joystick_types {
-	FLOATING, ##Joystick appears wherever the player touches.
-	FLOATING_BOTTOM, ##Joystick appears wherever the player touches if that touch is in the lower half of the screen.
-	FIXED_LEFT, ##Joystick is fixed to the bottom-left side of the screen. It will still float within that quadrant.
-	FIXED_RIGHT, ##Joystick is fixed to the bottom-right side of the screen. It will still float within that quadrant.
-	FIXED_CUSTOM ##Joystick is fixed to a custom position on the screen. Joystick will no longer float, but need to be touched in its specific position.
+	FLOATING, ##A single joystick appears wherever the player touches, and stays there until the next touch.
+	FLOATING_BOTTOM, ##A single joystick appears wherever the player touches if that touch is in the lower half of the screen, and it stays there until the next touch.
+	FLOATING_BOTTOM_LEFT, ##A single joystick appears wherever the player touches if that touch is in the bottom-left side of the screen.
+	FLOATING_BOTTOM_RIGHT, ##A single joystick appears wherever the player touches if that touch is in the bottom-right side of the screen.
+	FLOATING_DYNAMIC, ##A single joystick follows the player's touch constantly instead of staying put at the initial touch position.
+	FIXED_CUSTOM_SINGLE, ##A single joystick is fixed to a custom position on the screen. This joystick will no longer float, but only respond if touched at its specific position.
+	#TWIN_FLOATING, ##A left joystick appears whenever the player touches anywhere on the left side of the screen; a right joystick appears whenever the player touches anywhere on the right side of the screen.
+	#TWIN_FLOATING_BOTTOM, ##Same as TWIN_FLOATING except only applies to the bottom half of the screen.
+	#TWIN_FIXED ##A left and right joystick are fixed to the screen and will not activate unless directly pressed.
+	#FIXED_CUSTOM_TWIN, ##A left and right joystick are fixed to custom positions on the screen. These joysticks will not float, but only respond if touched at their specific positions.
 	}
 	
-@export var joystick_visible: bool = false
+@export var joystick_visibility: joystick_visibilities
+enum joystick_visibilities {
+	ON_TOUCH, ##Joystick only appears when the player is touching the screen,
+	ALWAYS, ##Joystick is always visible.
+	NEVER ##Joystick is never visible.
+	}
 
 var input_array: Array[PackedScene] = [
 	load("res://touch_joystick.tscn"),
@@ -19,6 +29,15 @@ func _ready() -> void:
 	setup_joystick()
 	
 func setup_joystick() -> void:
+	match joystick_visibility:
+		joystick_visibilities.ON_TOUCH:
+			%Touch_Joystick.visibility = %Touch_Joystick.visibilities.TOUCH
+		joystick_visibilities.ALWAYS:
+			%Touch_Joystick.visibility = %Touch_Joystick.visibilities.ALWAYS
+			%Touch_Joystick.set_visibility(false)
+		joystick_visibilities.NEVER:
+			%Touch_Joystick.visibility = %Touch_Joystick.visibilities.NEVER
+			%Touch_Joystick.set_visibility(false)
 	match joystick_type:
 		joystick_types.FLOATING:
 			%Touch_Joystick.pos_type = %Touch_Joystick.pos_types.FLOATING
@@ -26,13 +45,13 @@ func setup_joystick() -> void:
 		joystick_types.FLOATING_BOTTOM:
 			%Touch_Joystick.pos_type = %Touch_Joystick.pos_types.FLOATING
 			%Touch_Joystick.float_type = %Touch_Joystick.float_types.BOTTOM
-		joystick_types.FIXED_LEFT:
-			%Touch_Joystick.pos_type = %Touch_Joystick.pos_types.FIXED
+		joystick_types.FLOATING_BOTTOM_LEFT:
+			%Touch_Joystick.pos_type = %Touch_Joystick.pos_types.FLOATING
 			%Touch_Joystick.fixed_type = %Touch_Joystick.fixed_types.LEFT
-		joystick_types.FIXED_RIGHT:
-			%Touch_Joystick.pos_type = %Touch_Joystick.pos_types.FIXED
+		joystick_types.FLOATING_BOTTOM_RIGHT:
+			%Touch_Joystick.pos_type = %Touch_Joystick.pos_types.FLOATING
 			%Touch_Joystick.fixed_type = %Touch_Joystick.fixed_types.RIGHT
-		joystick_types.FIXED_CUSTOM:
+		joystick_types.FIXED_CUSTOM_SINGLE:
 			%Touch_Joystick.pos_type = %Touch_Joystick.pos_types.FIXED
 			%Touch_Joystick.fixed_type = %Touch_Joystick.fixed_types.CUSTOM
 			
--- touch_joystick.gd
+++ touch_joystick.gd
@@ -1,46 +1,77 @@
 extends CanvasLayer
 
+var visibility: visibilities
+enum visibilities {TOUCH,ALWAYS,NEVER}
+
 var pos_type: pos_types
 enum pos_types {FLOATING,FIXED}
 
 var float_type: float_types
-enum float_types {FULL,BOTTOM}
+enum float_types {FULL,BOTTOM,BOTTOM_LEFT,BOTTOM_RIGHT}
 
 var fixed_type: fixed_types
-enum fixed_types {LEFT,RIGHT,CUSTOM}
+enum fixed_types {SINGLE,TWIN}
 
-var mode = 1 # 0 = dynamic joystick, 1 = static joystick
+var base_mode: base_modes
+enum base_modes {STATIC,DYNAMIC}
+
 var radius = 35
 
 func _ready():
-	%Base.position = Vector2(-100, -100)
-	%Tracker.global_position = Vector2(-100, -100)
+	center_joystick_to_screen()
 
 func _input(event):
-	print(get_direction())
+	#print(get_direction())
 	if event is InputEventScreenTouch:
 		if event.is_pressed():
 			check_joystick_type(event)
+			set_visibility(true)
 		else:
-			%Base.position = Vector2(-100, -100)
-			%Tracker.global_position = Vector2(-100, -100)
+			center_joystick_to_screen()
+			set_visibility(false)
 
 	elif event is InputEventScreenDrag:
 		var base_center = %Base.position + %Base.texture.get_size() / 2
 		
-		if mode == 0:
-			center_tracker_to_touch(event.position)
-			if (event.position - base_center).length() >= radius:
-				var angle = (base_center - %Tracker.global_position).angle()
-				%Base.position = Vector2(radius * cos(angle), radius * sin(angle)) + %Tracker.global_position - %Base.texture.get_size() / 2
+		match base_mode:
+			base_modes.STATIC:
+				center_tracker_to_base(event.position)
+				
+			base_modes.DYNAMIC:
+				var tracker_size: Vector2 = %Tracker.texture.get_size()
+				%Tracker.global_position = event.position - tracker_size / 2
+				
+				var tracker_center: Vector2 = %Tracker.global_position + tracker_size / 2
+				var distance: Vector2 = (tracker_center - base_center).length()
+				
+				if distance > radius:
+					var direction: Vector2 = (tracker_center - base_center).normalized()
+					%Base.position = tracker_center - direction * radius - %Base.texture.get_size() / 2
 
-		if mode == 1:
-			center_tracker_to_base(event.position)
-
-func get_direction():
-	var 	base_center = %Base.position + %Base.texture.get_size() / 2
-	var tracker_center = %Tracker.global_position + %Tracker.texture.get_size() / 2
+func get_direction() -> Vector2:
+	var base_center: Vector2 = %Base.position + %Base.texture.get_size() / 2
+	var tracker_center: Vector2 = %Tracker.global_position + %Tracker.texture.get_size() / 2
 	return (tracker_center - base_center).normalized()
+	
+func set_visibility(make_visible:bool) -> void:
+	if make_visible:
+		match visibility:
+			visibilities.TOUCH:
+				%Base.visible = true
+			visibilities.ALWAYS:
+				%Base.visible = true
+			visibilities.NEVER:
+				%Base.visible = false
+	else:
+		match visibility:
+			visibilities.TOUCH:
+				%Base.visible = false
+			visibilities.ALWAYS:
+				print("am visible")
+				%Base.visible = true
+			visibilities.NEVER:
+				%Base.visible = false
+			
 	
 func check_joystick_type(event) -> void:
 	var screen_size: Vector2 = get_viewport().get_visible_rect().size
@@ -52,24 +83,36 @@ func check_joystick_type(event) -> void:
 				float_types.BOTTOM:
 					if event.position.y > screen_size.y /2:
 						center_tracker_to_touch(event.position)
-		pos_types.FIXED:
-			match fixed_type:
-				fixed_types.LEFT:
+				float_types.BOTTOM_LEFT:
 					if event.position.y > screen_size.y /2 and event.position.x < screen_size.x /2:
 						center_tracker_to_touch(event.position)
-				fixed_types.RIGHT:
+				float_types.BOTTOM_RIGHT:
+					if event.position.y > screen_size.y /2 and event.position.x > screen_size.x /2:
+						center_tracker_to_touch(event.position)
+					
+		pos_types.FIXED:
+			match fixed_type:
+				fixed_types.SINGLE:
 					pass
-				fixed_types.CUSTOM:
+				fixed_types.TWIN:
 					pass
 
 func center_tracker_to_touch(touch_pos: Vector2):
-	var base_size = %Base.texture.get_size()
-	var tracker_size = %Tracker.texture.get_size()
+	var base_size: Vector2 = %Base.texture.get_size()
+	var tracker_size: Vector2 = %Tracker.texture.get_size()
 	%Base.position = touch_pos - base_size / 2
 	%Tracker.global_position = touch_pos - tracker_size / 2
 
 func center_tracker_to_base(touch_pos: Vector2):
-	var base_center = %Base.position + %Base.texture.get_size() / 2
-	var direction = (touch_pos - base_center).limit_length(radius)
-	var tracker_size = %Tracker.texture.get_size()
+	var base_center: Vector2 = %Base.position + %Base.texture.get_size() / 2
+	var direction: Vector2 = (touch_pos - base_center).limit_length(radius)
+	var tracker_size: Vector2 = %Tracker.texture.get_size()
 	%Tracker.global_position = base_center + direction - tracker_size / 2
+	
+func center_joystick_to_screen():
+	var screen_center: Vector2 = get_viewport().get_visible_rect().size / 2
+	var base_size: Vector2 = %Base.texture.get_size()
+	var tracker_size: Vector2 = %Tracker.texture.get_size()
+	
+	%Base.position = screen_center - base_size / 2
+	%Tracker.global_position = screen_center - tracker_size / 2
